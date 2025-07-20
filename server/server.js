b01d83efe1c7b552691acabeff4045406e8dfe3d
Feyaz71 <feyazkhan8800@gmail.com>
2025-07-20 21:27:14 +0530

    Final commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server }); //chat App
 
 app.use(cors());
 
